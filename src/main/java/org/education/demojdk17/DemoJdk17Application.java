e2ebd11c7fcc9c89449d1f9394f20527e99f6767
st70r <st70r@hotmail.com>
2024-04-02 20:34:01 +0700

    init spring boot demo jdk17

---
+++ src/main/java/org/education/demojdk17/DemoJdk17Application.java
@@ -0,0 +1,13 @@
+package org.education.demojdk17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJdk17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJdk17Application.class, args);
+    }
+
+}
